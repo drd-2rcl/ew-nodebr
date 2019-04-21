355efc28b59155fb4311a64750aec7ba70d8762f
drd-2rcl <drd.rclrd@gmail.com>
2019-04-21 09:15:17 -0300

    write file async and register

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,5 +1,6 @@
 const {
-  readFile
+  readFile,
+  writeFile
 } = require('fs')
 
 const {
@@ -7,6 +8,7 @@ const {
 } = require('util')
 
 const readFileAsync = promisify(readFile)
+const writeFileAsync = promisify(writeFile)
 
 // outra forma de obtrer dados do json
 // const dadosJson = require('./herois.json')
@@ -22,9 +24,29 @@ class Database {
     return JSON.parse(arquivo.toString())
   }
 
-  escreverArquivo() {
-
+  async escreverArquivo(dados) {
+    await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+    return true
   }
+
+  async cadastrar(heroi) {
+    const dados = await this.obterDadosArquivo()
+    const id = heroi.id <= 2 ? heroi.id : Date.now();
+
+    const heroiComId = {
+      id,
+      ...heroi
+    }
+
+    const dadosFinal = [
+      ...dados,
+      heroiComId
+    ]
+
+    const resultado = await this.escreverArquivo(dadosFinal)
+    return resultado;
+  }
+
 
   async listar(id) {
     const dados = await this.obterDadosArquivo()
